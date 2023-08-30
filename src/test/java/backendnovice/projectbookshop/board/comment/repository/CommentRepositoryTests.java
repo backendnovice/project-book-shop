532cb893223f1913080aa713cbe0dffef56aa492
backendnovice <backendnovice@gmail.com>
2023-08-30 16:11:05 +0900

    :white_check_mark: 댓글 테스트 작성

---
+++ src/test/java/backendnovice/projectbookshop/board/comment/repository/CommentRepositoryTests.java
@@ -0,0 +1,89 @@
+/**
+ * @author    : backendnovice@gmail.com
+ * @date      : 2023-08-30
+ * @desc      : CommentRepository test class.
+ * @changelog :
+ * 2023-08-30 - backendnovice@gmail.com - create new file.
+ */
+
+package backendnovice.projectbookshop.board.comment.repository;
+
+import backendnovice.projectbookshop.board.article.domain.Article;
+import backendnovice.projectbookshop.board.article.repository.ArticleRepository;
+import backendnovice.projectbookshop.board.comment.domain.Comment;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class CommentRepositoryTests {
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    /**
+     * Initialize before all test. initialize with 150 fake comments
+     */
+    @BeforeAll
+    void initialize() {
+        for(int i = 1; i <= 3; i++) {
+            Article article = Article.builder()
+                    .title("Title #" + i)
+                    .content("Content #" + i)
+                    .writer("Writer")
+                    .build();
+
+            Article saved = articleRepository.save(article);
+
+            IntStream.rangeClosed(1, 50).forEach(j -> {
+                Comment comment = Comment.builder()
+                        .content("Comment #" + j)
+                        .writer("Writer #" + j)
+                        .article(article)
+                        .build();
+
+                commentRepository.save(comment);
+            });
+        }
+    }
+
+    @Test
+    void should_ReturnCommentTypePageObject_When_FindAllByArticleIdIsCalledAndSucceed() {
+        // given
+        Pageable pageable = PageRequest.of(0, 10);
+        Long articleId = 1L;
+
+        // when
+        Page<Comment> result = commentRepository.findAllByArticleId(articleId, pageable);
+
+        // then
+        assertThat(result.isEmpty()).isFalse();
+        assertThat(result.getTotalPages()).isEqualTo(5);
+        assertThat(result.getTotalElements()).isEqualTo(50);
+    }
+
+    @Test
+    void should_ReturnEmptyObject_When_FindAllByArticleIdIsCalledAndFailed() {
+        // given
+        Pageable pageable = PageRequest.of(0, 10);
+        Long articleId = 4L;
+
+        // when
+        Page<Comment> result = commentRepository.findAllByArticleId(articleId, pageable);
+
+        // then
+        assertThat(result.isEmpty());
+    }
+}
---
+++ src/test/java/backendnovice/projectbookshop/board/comment/service/CommentServiceTests.java
@@ -0,0 +1,155 @@
+/**
+ * @author    : backendnovice@gmail.com
+ * @date      : 2023-08-30
+ * @desc      : CommentService test class.
+ * @changelog :
+ * 2023-08-30 - backendnovice@gmail.com - create new file.
+ */
+
+package backendnovice.projectbookshop.board.comment.service;
+
+import backendnovice.projectbookshop.board.comment.domain.Comment;
+import backendnovice.projectbookshop.board.comment.dto.CommentDTO;
+import backendnovice.projectbookshop.board.comment.repository.CommentRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class CommentServiceTests {
+    @Mock
+    private CommentRepository commentRepository;
+
+    @InjectMocks
+    private CommentServiceImpl commentService;
+
+    private Page<Comment> fakeComments;
+
+    private Page<Comment> emptyComments;
+
+    @BeforeAll
+    void initialize() {
+        List<Comment> commentList = new ArrayList<>();
+        commentList.add(new Comment());
+        fakeComments = new PageImpl<>(commentList);
+        emptyComments = Page.empty();
+    }
+
+    @Test
+    void should_ReturnCommentDTOTypePageObject_When_GetCommentsIsCalledAndSucceed() {
+        // given
+        Pageable pageable = PageRequest.of(0, 10);
+        Long articleId = 1L;
+
+        // when
+        when(commentRepository.findAllByArticleId(anyLong(), any(Pageable.class))).thenReturn(fakeComments);
+
+        // then
+        Page<CommentDTO> result = commentService.getComments(articleId, pageable);
+        assertThat(result).isNotNull();
+    }
+
+    @Test
+    void should_ThrowNoSuchElementException_When_GetCommentsIsCalledAndFailed() {
+        // given
+        Pageable pageable = PageRequest.of(0, 10);
+        Long articleId = 1L;
+
+        // when
+        when(commentRepository.findAllByArticleId(anyLong(), any(Pageable.class))).thenReturn(emptyComments);
+
+        // then
+        assertThatThrownBy(() -> {
+            Page<CommentDTO> result = commentService.getComments(articleId, pageable);
+        }).isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    void should_CalledSaveOneTimes_When_WriteIsCalledAndSucceed() {
+        // given
+        CommentDTO commentDTO = mock(CommentDTO.class);
+        Comment comment = mock(Comment.class);
+
+        // when
+        when(commentRepository.save(any(Comment.class))).thenReturn(comment);
+        commentService.write(commentDTO);
+
+        // then
+        verify(commentRepository, times(1)).save(any(Comment.class));
+    }
+
+    @Test
+    void should_CalledSaveOneTimes_When_ModifyIsCalledAndSucceed() {
+        // given
+        CommentDTO commentDTO = mock(CommentDTO.class);
+        Comment comment = mock(Comment.class);
+
+        // when
+        when(commentRepository.findById(anyLong())).thenReturn(Optional.ofNullable(comment));
+        when(commentRepository.save(any(Comment.class))).thenReturn(comment);
+        commentService.modify(commentDTO);
+
+        // then
+        verify(commentRepository, times(1)).findById(anyLong());
+        verify(commentRepository, times(1)).save(any(Comment.class));
+    }
+
+    @Test
+    void should_ThrowNoSuchElementException_When_ModifyIsCalledAndFailed() {
+        // given
+        CommentDTO commentDTO = mock(CommentDTO.class);
+
+        // when
+        when(commentRepository.findById(anyLong())).thenThrow(NoSuchElementException.class);
+
+        // then
+        assertThatThrownBy(() -> {
+            commentService.modify(commentDTO);
+        }).isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    void should_CalledDeleteByIdOneTimes_When_DeleteIsCalledAndSucceed() {
+        // given
+        Long commentId = 1L;
+
+        // when
+        doNothing().when(commentRepository).deleteById(anyLong());
+        commentService.delete(commentId);
+
+        // then
+        verify(commentRepository, times(1)).deleteById(commentId);
+    }
+
+    @Test
+    void should_ThrowNoSuchElementException_When_DeleteIsCalledAndFailed() {
+        // given
+        Long commentId = 1L;
+
+        // when
+        doThrow(EmptyResultDataAccessException.class).when(commentRepository).deleteById(commentId);
+
+        // then
+        assertThatThrownBy(() -> {
+            commentService.delete(commentId);
+        }).isInstanceOf(NoSuchElementException.class);
+    }
+}
